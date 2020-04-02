5f3fc0d92da8ccc0229c12644d75f5f252d18191
guyburin <guyfreedom02@gmail.com>
2020-04-02 22:39:08 +0700

    edit report

--- application/controllers/ReportScores.php
+++ application/controllers/ReportScores.php
@@ -3,13 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ReportScores extends BD_Controller {
 
-	public function index()
+	public function people()
 	{
 		$this->load->view('layout/head');
 		$this->load->view('layout/head');
 		$this->load->view('layout/header');
 		$this->load->view('layout/left-menu');
-		$this->load->view('report_score/reportscore1');
+		$this->load->view('report_score/people');
+		// $this->load->view('layout/footer');
+		$this->load->view('layout/foot');
+
+	}
+
+	public function youth()
+	{
+		$this->load->view('layout/head');
+		$this->load->view('layout/head');
+		$this->load->view('layout/header');
+		$this->load->view('layout/left-menu');
+		$this->load->view('report_score/youth');
 		// $this->load->view('layout/footer');
 		$this->load->view('layout/foot');
 
---
+++ application/views/report_score/people.php
@@ -0,0 +1,71 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h2>รายงานผลการแข่งขัน</h2>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">รายงานผลการแข่งขัน</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <br>
+    <section class="content">
+    <label for="generation">ประเภทการแข่งขัน :</label>
+      <select id="generation">
+        <option value="national">ประชาชน</option>
+        <option value="Youth">เยาวชน</option>
+      </select>
+    
+    <label for="competition_type"> รุ่นผู้แข่งขัน :</label>
+      <select id="competition_type">
+        <option value="handN">มือ N</option>
+        <option value="handS-">มือ S-</option>
+        <option value="handS+">มือ S+</option>
+        <option value="handP-">มือ P-</option>
+        <option value="handP+C">มือ P+C</option>
+      </select>
+      <button>ค้นหา</button>
+      <br><br>
+      <div style="overflow-x:auto;">
+        <table>
+          <tr>
+            <th>คนที่ 1</th>
+            <th>คนที่ 2</th>
+            <th>คะแนนรอบที่ 1</th>
+            <th>คะแนนรอบที่ 2</th>
+            <th>คะแนนรอบที่ 3</th>
+            <th>ผู้ชนะ</th>
+          </tr>
+          <tr>
+            <td>อคาเชีย</td>
+            <td>โทริโกะ</td>
+            <td>9 - 10</td>
+            <td>7 - 10</td>
+            <td>-</td>
+            <td>โทริโกะ</td>
+          </tr>
+          <tr>
+            <td>ไซตามะ</td>
+            <td>โงกุน</td>
+            <td>999 - 0</td>
+            <td> - </td>
+            <td>  - </td>
+            <td>ไซตามะ</td>
+          </tr>
+          <tr>
+            <td>ไซตามะ</td>
+            <td>โงกุน</td>
+            <td>999 - 0</td>
+            <td> - </td>
+            <td>  - </td>
+            <td>ไซตามะ</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+ 
